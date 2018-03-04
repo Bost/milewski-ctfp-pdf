f0688b74eb06615fa3c56a2d2a65c619dfc6cce4
Bost <thebost@gmail.com>
2018-03-04 23:41:16 +0100

    Template

---
+++ src/content/klipse_settings.js
@@ -0,0 +1,13 @@
+window.klipse_settings = {
+    selector: '.language-klipse', // css selector for the html elements you want to klipsify
+    codemirror_options_in: {
+        indentUnit: 4,
+        lineWrapping: true,
+        lineNumbers: true,
+        autoCloseBrackets: true
+    },
+    codemirror_options_out: {
+        lineWrapping: true,
+        lineNumbers: true
+    }
+}
